87ecc1d768f24cef6c819ffa2bd4c4d8e4023888
cliff-mathebula <47654155@mylife.unisa.ac.za>
2020-10-23 06:10:03 +0200

    feat: added the cart totals to proceed to checkout

--- cart.php
+++ cart.php
@@ -328,6 +328,7 @@ $db_handle = new DBController();
         }
     } elseif ($_GET['action'] ==  "empty") {
         unset($_SESSION["cart_item"]);
+        header('LOCATION:cart.php');
     }
 
     ?>
@@ -351,10 +352,13 @@ $db_handle = new DBController();
                         <table class="table-shopping-cart">
                             <tr class="table-head">
                                 <th class="column-1">Product Name</th>
-                                <th class="column-3">Unit Price</th>
-                                <th class="column-4">Quantity</th>
-                                <th class="column-5">Total</th>
-                                <th class="column-5"> <a id="btnEmpty" href="cart.php?action=empty">Empty Cart</a>
+                                <th class="column-3 text-center">Unit Price</th>
+                                <th class="column-4 text-center">Quantity</th>
+                                <th class="column-5 text-center">Total</th>
+                                <th class="column-5"> 
+                                    <a id="btnEmpty" href="cart.php?action=empty"
+                                     class="flex-c-m sizefull bg-danger bo-rad-23 hov1 text-white trans-0-4">
+                                     <small><strong>Empty Cart</strong></small></a>
                                 </th>
                             </tr>
 
@@ -370,23 +374,15 @@ $db_handle = new DBController();
                                         </div>
                                     </td>
 
-                                    <td class="column-2"><?php echo "R " . $item["price_per_month"]; ?></td>
-                                    <td class="column-3">
-                                        <div class="flex-w bo5 of-hidden w-size17">
-                                            <button class="btn-num-product-down color1 flex-c-m size7 bg8 eff2">
-                                                <i class="fs-12 fa fa-minus" aria-hidden="true"></i>
-                                            </button>
-
-                                            <input class="size8 m-text18 t-center num-product" type="number" name="num-product2" value="<?php echo $item["quantity"]; ?>">
-
-                                            <button class="btn-num-product-up color1 flex-c-m size7 bg8 eff2">
-                                                <i class="fs-12 fa fa-plus" aria-hidden="true"></i>
-                                            </button>
+                                    <td class="column-2 text-center"><?php echo "R " . $item["price_per_month"]; ?></td>
+                                    <td class="column-3 text-center">
+                                        <div class="column-4 text-center">
+                                            <?php echo $item["quantity"]; ?>
                                         </div>
                                     </td>
-                                    <td class="column-4"><?php echo "R " . number_format($item_price, 2); ?></td>
+                                    <td class="column-5"><?php echo "R " . number_format($item_price, 2); ?></td>
 
-                                    <td column="column-5">
+                                    <td column="column-6">
                                         <div class="w-size17">
                                             <a href="cart.php?action=remove&code=<?php echo $item["code"]; ?>"><img src="icon-delete.png" alt="Remove Item" /></a>
                                         </div>
@@ -398,22 +394,7 @@ $db_handle = new DBController();
                                 $total_price += ($item["price_per_month"] * $item["quantity"]);
                             }
                             ?>
-
-                            <tr>
-                                <td colspan="2" align="right">Total:</td>
-                                <td align="right"><?php echo $total_quantity; ?></td>
-                                <td align="right" colspan="2"><strong><?php echo "R " . number_format($total_price, 2); ?></strong></td>
-                                <td></td>
-                            </tr>
                         </table>
-
-                    <?php
-                    } else {
-                    ?>
-                        <div class="no-records">Your Cart is Empty</div>
-                    <?php
-                    }
-                    ?>
 
                 </div>
             </div>
@@ -431,23 +412,11 @@ $db_handle = new DBController();
                         </button>
                     </div>
                 </div>
-
-                <div class="size10 trans-0-4 m-t-10 m-b-10">
-                    <!-- Button -->
-                    <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-                        Update Cart
-                    </button>
-                </div>
             </div>
 
 
-
-
-
-
-
             <!-- Total -->
-            <div class="bo9 w-size18 p-l-40 p-r-40 p-t-30 p-b-38 m-t-30 m-r-0 m-l-auto p-lr-15-sm">
+            <div class="bo8 w-size18 p-l-40 p-r-40 p-t-30 p-b-38 m-t-30 m-r-0 m-l-auto p-lr-15-sm">
                 <h5 class="m-text20 p-b-24">
                     Cart Totals
                 </h5>
@@ -459,48 +428,20 @@ $db_handle = new DBController();
                     </span>
 
                     <span class="m-text21 w-size20 w-full-sm">
-                        $39.00
+                        <?php echo "R " . number_format($total_price, 2); ?>
                     </span>
                 </div>
 
                 <!--  -->
                 <div class="flex-w flex-sb bo10 p-t-15 p-b-20">
                     <span class="s-text18 w-size19 w-full-sm">
-                        Shipping:
+                        Quantity:
                     </span>
 
                     <div class="w-size20 w-full-sm">
-                        <p class="s-text8 p-b-23">
-                            There are no shipping methods available. Please double check your address, or contact us if you need any help.
+                        <p class="m-text21 w-size20 w-full-sm">
+                            <?php echo $total_quantity; ?>
                         </p>
-
-                        <span class="s-text19">
-                            Calculate Shipping
-                        </span>
-
-                        <div class="rs2-select2 rs3-select2 rs4-select2 bo4 of-hidden w-size21 m-t-8 m-b-12">
-                            <select class="selection-2" name="country">
-                                <option>Select a country...</option>
-                                <option>US</option>
-                                <option>UK</option>
-                                <option>Japan</option>
-                            </select>
-                        </div>
-
-                        <div class="size13 bo4 m-b-12">
-                            <input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="state" placeholder="State /  country">
-                        </div>
-
-                        <div class="size13 bo4 m-b-22">
-                            <input class="sizefull s-text7 p-l-15 p-r-15" type="text" name="postcode" placeholder="Postcode / Zip">
-                        </div>
-
-                        <div class="size14 trans-0-4 m-b-10">
-                            <!-- Button -->
-                            <button class="flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4">
-                                Update Totals
-                            </button>
-                        </div>
                     </div>
                 </div>
 
@@ -511,7 +452,9 @@ $db_handle = new DBController();
                     </span>
 
                     <span class="m-text21 w-size20 w-full-sm">
-                        $39.00
+                        <?php $total = number_format($total_price, 2) + number_format($total_price, 2) * 0.15; 
+                        echo 'R'.number_format($total, 2);
+                        ?>
                     </span>
                 </div>
 
@@ -523,7 +466,16 @@ $db_handle = new DBController();
                 </div>
             </div>
         </div>
+
+    <?php
+                    } else {
+    ?>
+        <div class="alert alert-danger text-center">Your Cart is Empty</div>
+    <?php
+                    }
+    ?>
     </section>
+
 
 
 
