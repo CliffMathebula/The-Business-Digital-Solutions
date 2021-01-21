40608f55cd6d870c5e0abb2b301c58264adfef4b
Cliff <climathe1@gmail.com>
2021-01-21 07:46:38 -0800

    refactor modified the dbcontrollers to not use database connection password and also deleted the zoom sdk folder as it was no longer needed

--- dbcontroller.php
+++ dbcontroller.php
@@ -2,7 +2,7 @@
 class DBController {
 	private $host = "localhost";
 	private $user = "root";
-	private $password = "1989@Cliff";
+	private $password = "";
 	private $database = "tbds_hosting";
 	private $conn;
 	
